ffeabee6e77e6565070d42239f76b4082893e465
mypace <love_kyokos@yahoo.com.tw>
2018-01-09 14:05:49 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,6 +1,7 @@
 <?php
 $json_str = file_get_contents('php://input'); //接收REQUEST的BODY
 $json_obj = json_decode($json_str); //轉JSON格式
+printf $json_obj;
 //產生回傳給line server的格式
 $sender_userid = $json_obj->events[0]->source->userId;
 $sender_txt = $json_obj->events[0]->message->text;
@@ -14,7 +15,7 @@ $response = array (
 		)
 	);
 $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
-fwrite($myfile, "\xEF\xBB\xBF".$json_obj.PHP_EOL.json_encode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+fwrite($myfile, "\xEF\xBB\xBF".$json_str.PHP_EOL.json_encode($response)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
 fclose($myfile);
 //回傳給line server
 $header[] = "Content-Type: application/json";
