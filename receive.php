15501e3db1299f66eebd0d87ebae34ed9778354e
mypace <love_kyokos@yahoo.com.tw>
2018-01-09 14:53:28 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -45,6 +45,22 @@
 				)
 			);
         		break;
+		case "mypic":
+			$line_server_url = 'https://api.line.me/v2/bot/profile/'.$sender_userid;
+ 			$header[] = "Content-Type: application/json";
+ 			$header[] = "Authorization: Bearer fmUCBqFv8uF0YAIfJUE2uEtUFNnhqP/Vd5IqdgnKYPSyYC7/rqsszpwRMjCRrAyk2pbzTMz1NP77A0AOlQQN0JMIeUr6gCEmp2y9aSHW2klseVMC9/Om9yBOXoBKOriG+2s0r+VOUN3+Hl92wzXCNwdB04t89/1O/w1cDnyilFU=";
+ 			$ch = curl_init($line_server_url);                                                                      
+ 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+ 			curl_setopt($ch, CURLOPT_POSTFIELDS,"");                                                                  
+ 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+ 			curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
+ 			$result = curl_exec($ch);
+ 			curl_close($ch); 
+		 	$json_obj = json_decode($result); //轉JSON格式
+		 	$myfile2 = fopen("log2.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
+ 			fwrite($myfile2, "\xEF\xBB\xBF".json_encode($result)); //在字串前加入\xEF\xBB\xBF轉成utf8格式
+ 			fclose($myfile2);
+        		break;
  }
 
  $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個log.txt 用來印訊息
