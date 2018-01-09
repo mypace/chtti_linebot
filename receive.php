48a374f55faaae3b2de243591606d7c8d31fc4e8
mypace <love_kyokos@yahoo.com.tw>
2018-01-09 12:01:13 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -3,6 +3,6 @@
   $json_obj=json_decode($json_str); //轉為json格式
 
   $myfile=fopen("log.txt","w+") or die("Unable to open file");
-  fwrite($myfile, $json_str.PHP_EOL);  //在字串前加入\xEF\xBB\xBF 轉成utf-8格式
+  fwrite($myfile, "\xEF\xBB\xBF".$json_str.PHP_EOL);  //在字串前加入\xEF\xBB\xBF 轉成utf-8格式
   fclose($myfile);
 ?>
